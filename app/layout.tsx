31b55a4e4b19c1207a7cc9c323ebbe410b69138a
danielquillanroxas <casrox54@yahoo.com>
2023-08-24 11:06:14 +0300

    theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head><link id="theme-link" rel="stylesheet" href="/themes/lara-light-blue/theme.css" /></Head>
       <body className={inter.className}>
         {children}
         </body>
